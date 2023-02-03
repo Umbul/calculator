826e581945067b0fb85ca6e22a9d97cc4227c396
umbul <umbulbanin9@gmail.com>
2023-02-03 15:48:00 -0500

    branch1 changes

---
+++ Javascript/QUIZ/task17.js
@@ -0,0 +1,8 @@
+
+
+
+const dive = document.createElement("div");
+dive.innerHTML = "This is a div. hrough this letter about your advertisement published on <br>indeed regarding a vacancy for a post<br> of full stack developer in your prestigious company, I would be glad to apply for this wonderful post.";
+
+
+document.body.appendChild(dive);
